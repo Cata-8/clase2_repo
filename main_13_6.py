14f2729ed2f8edae6dc158d2ce0dafd6ee6f9e90
Cata-8 <catalinaalmuna01@gmail.com>
2025-06-13 15:33:01 -0400

    Funcionalidad suma ok

--- main_13_6.py
+++ main_13_6.py
@@ -1,14 +1,19 @@
-print("Hola 1")
-print("Hola 2")
-print("Hola 3")
-print("Hola 3")
-print("Hola 3")
-print("Hola 3")
-print("Hola 3")
-print("Hola 4")
-print("Hola 4")
-print("Hola 4")
-print("Hola 4")
-print("Hola 4")
-print("Hola 4")
-print("Hola 4")
+print("Calculadora")
+print("1. Sumar")
+print("2. Restar")
+print("3. Multiplicar")
+print("4. Dividir")
+print("5. Salir")
+
+resultado = 0
+
+while True:
+    opt = int(input("Ingrese la opcion: "))
+    num1 = int(input("Ingrese el primer numero: "))
+    num2 = int(input("Ingrese el segundo numero: "))
+    
+    if opt == 1:
+        resultado = num1 + num2
+        print(f"El reusltado es: ")
+    elif opt == 2:
+        
